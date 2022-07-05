9ae259a014f975c67e01f0a9c6d2d4c8f438b909
mano <mano.24.rites@gmail.com>
2022-07-05 13:35:02 +0900

    線、点の表示切替を追加、gitキャッシュ削除

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from json import tool
 import tkinter
+from turtle import color
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 import matplotlib.animation as animation
@@ -10,8 +11,10 @@ import func
 
 p_count = 20
 active = False
-interval = 1000
-frames = int(10000 / interval)
+interval = 100
+frames = int(100000 / interval)
+line = True
+plot = True
 x_data = []
 y_data = []
 
@@ -29,18 +32,21 @@ def update(frame):
     x_data.append(frame)
     y = func.data(frame)
     y_data.append(y)
+    ax.cla()
     if (len(x_data) > p_count):
-        ax.lines.pop(0)
         x_data.pop(0)
         y_data.pop(0)
         ax.set_xlim(x_data[0]-0.5, x_data[len(x_data)-1]+0.5)
         ax.set_ylim(min(y_data)-0.5, max(y_data)+0.5)
-    ax.plot(frame, y, "o")
+    if line:
+        ax.plot(x_data, y_data, color = "blue")
+    if plot:
+        ax.plot(x_data, y_data, "o")
 
 def _set_interval():
     global interval, frames
     interval = int(tf_inter.get())
-    frames = int(10000 / interval)
+    frames = int(100000 / interval)
 
 def _set_plc():
     global p_count
@@ -65,6 +71,14 @@ def _save_gif():
     global ani
     ani.save("test.gif", writer = 'pillow')
 
+def _show_line():
+    global line
+    line = not line
+
+def _show_plot():
+    global plot
+    plot = not plot
+
 # ----------------------------------------
 
 root = tkinter.Tk()
@@ -74,6 +88,7 @@ canvas = FigureCanvasTkAgg(fig, master=root)
 toolbar = NavigationToolbar2Tk(canvas, root)
 ani = None
 ax = fig.add_subplot(111)
+ax.grid()
 canvas.get_tk_widget().pack()
 
 stop_button = tkinter.Button(master=root, text="START", command=_init_anim)
@@ -94,6 +109,11 @@ inter_button.pack(side=tkinter.LEFT)
 tf_plc = tkinter.Entry(width=20)
 tf_plc.insert(tkinter.END,"20")
 tf_plc.pack(side=tkinter.LEFT)
+
+stop_button = tkinter.Button(master=root, text="line", command=_show_line)
+stop_button.pack(side=tkinter.LEFT)
+stop_button = tkinter.Button(master=root, text="plot", command=_show_plot)
+stop_button.pack(side=tkinter.LEFT)
 inter_button = tkinter.Button(master=root, text="SaveGIF", command=_save_gif)
 inter_button.pack(side=tkinter.LEFT)
 tkinter.mainloop()
