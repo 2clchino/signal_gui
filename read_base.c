a40d02f0d419a2919448a8af04ed791fcc2b43f0
Shun Kasai <mano2@DESKTOP-0SOSQ9P.localdomain>
2022-07-14 02:29:37 +0900

    ベースのソースを保持

--- read_base.c
+++ read_base.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 #include <sys/types.h>
-// #include <unistd.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdint.h>
 
-int64_t* value(void);
-int64_t buf[32];
-
-int main(void){
-  int64_t* buf=value();
-  for (int i=0; i<32; i++)
-    printf("%02x: %016lx\n", i, buf[i]);
-}
-
-int64_t* value(void){
-  /*
+int main(){
   int fd = open("/dev/xdma0_c2h_0", O_RDONLY);
   printf("FD: %d\n", fd);
-  
+
   unsigned int pos = lseek(fd, 0xc0000000, SEEK_SET); // BRAM
+
   printf("%x\n", pos);
-  
+
+  int64_t buf[32];
   int len = read(fd, buf, 32*8);
   close(fd);
+
   printf("Len: %d\n", len);
-  */
+
   for (int i=0; i<32; i++)
-    buf[i] = i*2;
-  return buf;
+    printf("%02x: %016llx\n", i, buf[i]);
+
 }
